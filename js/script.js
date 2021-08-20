5f211af8b40075f1df69abb745a585f085b27326
Rancour666 <77494784+Rancour666@users.noreply.github.com>
2021-08-20 18:41:15 +0300

    RanCour-Practice-1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//BURGER BUTTON
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+
+//IBG
+function ibg(){
+$.each($('.ibg'), function(index, val) {
+if($(this).find('img').length>0){
+$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+}
+});
+}
+
+ibg();
